dc2a431f77fac885b27a1571bdb37d97776f0f63
kolobok kelbek <dev_kelbek@mail.ru>
2023-08-19 19:06:58 +0300

    add test with huge config file

--- loader_test.go
+++ loader_test.go
@@ -27,11 +27,12 @@ func Test_Loader_Load(t *testing.T) {
 	config, err := loader.Load("hello", YamlExt, "./testdata/loadYaml")
 
 	as.Nil(err)
-	as.NotNil(config)
 
-	as.Equal(config.ServerName, "HelloWorld")
-	as.Equal(config.Port, 80)
-	as.Equal(config.Timeout, 20)
+	as.Equal(config, &TestConfig{
+		ServerName: "HelloWorld",
+		Port:       80,
+		Timeout:    20,
+	})
 }
 
 func Test_Loader_Load_withEnvReplace(t *testing.T) {
@@ -51,30 +52,33 @@ func Test_Loader_Load_withEnvReplace(t *testing.T) {
 	config, err := loader.Load("hello", YamlExt, "./testdata/loadYaml")
 
 	as.Nil(err)
-	as.NotNil(config)
-
-	as.Equal(config.ServerName, "PAM-PAM")
-	as.Equal(config.Port, 8080)
-	as.Equal(config.Timeout, 20)
+	as.Equal(config, &TestConfig{
+		ServerName: "PAM-PAM",
+		Port:       8080,
+		Timeout:    20,
+	})
 }
 
 func Test_Loader_LoadFromDir(t *testing.T) {
 	as := assert.New(t)
 
+	type App struct {
+		Name        string
+		Description string
+	}
+	type Db struct {
+		Port    int
+		Timeout int
+	}
+	type Server struct {
+		Name    string
+		Port    int
+		Timeout int
+	}
 	type TestConfig struct {
-		App struct {
-			Name        string
-			Description string
-		}
-		Db struct {
-			Port    int
-			Timeout int
-		}
-		Server struct {
-			Name    string
-			Port    int
-			Timeout int
-		}
+		App    App
+		Db     Db
+		Server Server
 	}
 
 	loader := NewLoader[TestConfig]()
@@ -82,15 +86,21 @@ func Test_Loader_LoadFromDir(t *testing.T) {
 	config, err := loader.LoadFromDir("hello", "./testdata/loadDirYaml", YamlExt)
 
 	as.Nil(err)
-	as.NotNil(config)
-
-	as.Equal(config.App.Name, "HelloWorld")
-	as.Equal(config.App.Description, "This is gorgeous application")
-	as.Equal(config.Db.Port, 3036)
-	as.Equal(config.Db.Timeout, 10)
-	as.Equal(config.Server.Name, "ServerName")
-	as.Equal(config.Server.Port, 80)
-	as.Equal(config.Server.Timeout, 20)
+	as.Equal(config, &TestConfig{
+		App: App{
+			Name:        "HelloWorld",
+			Description: "This is gorgeous application",
+		},
+		Db: Db{
+			Port:    3036,
+			Timeout: 10,
+		},
+		Server: Server{
+			Name:    "ServerName",
+			Port:    80,
+			Timeout: 20,
+		},
+	})
 }
 
 func Test_Loader_LoadFromDir_withEnvReplace(t *testing.T) {
@@ -100,20 +110,23 @@ func Test_Loader_LoadFromDir_withEnvReplace(t *testing.T) {
 	t.Setenv("HELLO_SERVER_NAME", "PAM-PAM")
 	t.Setenv("HELLO_SERVER_PORT", "8080")
 
+	type App struct {
+		Name        string
+		Description string
+	}
+	type Db struct {
+		Port    int
+		Timeout int
+	}
+	type Server struct {
+		Name    string
+		Port    int
+		Timeout int
+	}
 	type TestConfig struct {
-		App struct {
-			Name        string
-			Description string
-		}
-		Db struct {
-			Port    int
-			Timeout int
-		}
-		Server struct {
-			Name    string
-			Port    int
-			Timeout int
-		}
+		App    App
+		Db     Db
+		Server Server
 	}
 
 	loader := NewLoader[TestConfig]()
@@ -121,34 +134,43 @@ func Test_Loader_LoadFromDir_withEnvReplace(t *testing.T) {
 	config, err := loader.LoadFromDir("hello", "./testdata/loadDirYaml", YamlExt)
 
 	as.Nil(err)
-	as.NotNil(config)
-
-	as.Equal(config.App.Name, "APP-NAME")
-	as.Equal(config.App.Description, "This is gorgeous application")
-	as.Equal(config.Db.Port, 3036)
-	as.Equal(config.Db.Timeout, 10)
-	as.Equal(config.Server.Name, "PAM-PAM")
-	as.Equal(config.Server.Port, 8080)
-	as.Equal(config.Server.Timeout, 20)
+	as.Equal(config, &TestConfig{
+		App: App{
+			Name:        "APP-NAME",
+			Description: "This is gorgeous application",
+		},
+		Db: Db{
+			Port:    3036,
+			Timeout: 10,
+		},
+		Server: Server{
+			Name:    "PAM-PAM",
+			Port:    8080,
+			Timeout: 20,
+		},
+	})
 }
 
 func Test_Loader_LoadFromEmbed(t *testing.T) {
 	as := assert.New(t)
 
+	type App struct {
+		Name        string
+		Description string
+	}
+	type Db struct {
+		Port    int
+		Timeout int
+	}
+	type Server struct {
+		Name    string
+		Port    int
+		Timeout int
+	}
 	type TestConfig struct {
-		App struct {
-			Name        string
-			Description string
-		}
-		Db struct {
-			Port    int
-			Timeout int
-		}
-		Server struct {
-			Name    string
-			Port    int
-			Timeout int
-		}
+		App    App
+		Db     Db
+		Server Server
 	}
 
 	loader := NewLoader[TestConfig]()
@@ -156,15 +178,21 @@ func Test_Loader_LoadFromEmbed(t *testing.T) {
 	config, err := loader.LoadFromEmbedFS("hello", embedTestData, YamlExt)
 
 	as.Nil(err)
-	as.NotNil(config)
-
-	as.Equal(config.App.Name, "HelloWorld")
-	as.Equal(config.App.Description, "This is gorgeous application")
-	as.Equal(config.Db.Port, 3036)
-	as.Equal(config.Db.Timeout, 10)
-	as.Equal(config.Server.Name, "ServerName")
-	as.Equal(config.Server.Port, 80)
-	as.Equal(config.Server.Timeout, 20)
+	as.Equal(config, &TestConfig{
+		App: App{
+			Name:        "HelloWorld",
+			Description: "This is gorgeous application",
+		},
+		Db: Db{
+			Port:    3036,
+			Timeout: 10,
+		},
+		Server: Server{
+			Name:    "ServerName",
+			Port:    80,
+			Timeout: 20,
+		},
+	})
 }
 
 func Test_Loader_LoadFromEmbed_withEnvReplace(t *testing.T) {
@@ -174,20 +202,23 @@ func Test_Loader_LoadFromEmbed_withEnvReplace(t *testing.T) {
 	t.Setenv("HELLO_SERVER_NAME", "PAM-PAM")
 	t.Setenv("HELLO_SERVER_PORT", "8080")
 
+	type App struct {
+		Name        string
+		Description string
+	}
+	type Db struct {
+		Port    int
+		Timeout int
+	}
+	type Server struct {
+		Name    string
+		Port    int
+		Timeout int
+	}
 	type TestConfig struct {
-		App struct {
-			Name        string
-			Description string
-		}
-		Db struct {
-			Port    int
-			Timeout int
-		}
-		Server struct {
-			Name    string
-			Port    int
-			Timeout int
-		}
+		App    App
+		Db     Db
+		Server Server
 	}
 
 	loader := NewLoader[TestConfig]()
@@ -195,35 +226,43 @@ func Test_Loader_LoadFromEmbed_withEnvReplace(t *testing.T) {
 	config, err := loader.LoadFromEmbedFS("hello", embedTestData, YamlExt)
 
 	as.Nil(err)
-	as.NotNil(config)
-
-	as.Equal(config.App.Name, "APP-NAME")
-	as.Equal(config.App.Description, "This is gorgeous application")
-	as.Equal(config.Db.Port, 3036)
-	as.Equal(config.Db.Timeout, 10)
-	as.Equal(config.Server.Name, "PAM-PAM")
-	as.Equal(config.Server.Port, 8080)
-	as.Equal(config.Server.Timeout, 20)
+	as.Equal(config, &TestConfig{
+		App: App{
+			Name:        "APP-NAME",
+			Description: "This is gorgeous application",
+		},
+		Db: Db{
+			Port:    3036,
+			Timeout: 10,
+		},
+		Server: Server{
+			Name:    "PAM-PAM",
+			Port:    8080,
+			Timeout: 20,
+		},
+	})
 }
-
 
 func Test_Loader_LoadFromEmbedByPath(t *testing.T) {
 	as := assert.New(t)
 
+	type App struct {
+		Name        string
+		Description string
+	}
+	type Db struct {
+		Port    int
+		Timeout int
+	}
+	type Server struct {
+		Name    string
+		Port    int
+		Timeout int
+	}
 	type TestConfig struct {
-		App struct {
-			Name        string
-			Description string
-		}
-		Db struct {
-			Port    int
-			Timeout int
-		}
-		Server struct {
-			Name    string
-			Port    int
-			Timeout int
-		}
+		App    App
+		Db     Db
+		Server Server
 	}
 
 	loader := NewLoader[TestConfig]()
@@ -231,13 +270,211 @@ func Test_Loader_LoadFromEmbedByPath(t *testing.T) {
 	config, err := loader.LoadFromEmbedFSByPath("hello", embedFullTestData, "testdata/loadDirYaml", YamlExt)
 
 	as.Nil(err)
-	as.NotNil(config)
+	as.Equal(config, &TestConfig{
+		App: App{
+			Name:        "HelloWorld",
+			Description: "This is gorgeous application",
+		},
+		Db: Db{
+			Port:    3036,
+			Timeout: 10,
+		},
+		Server: Server{
+			Name:    "ServerName",
+			Port:    80,
+			Timeout: 20,
+		},
+	})
+}
 
-	as.Equal(config.App.Name, "HelloWorld")
-	as.Equal(config.App.Description, "This is gorgeous application")
-	as.Equal(config.Db.Port, 3036)
-	as.Equal(config.Db.Timeout, 10)
-	as.Equal(config.Server.Name, "ServerName")
-	as.Equal(config.Server.Port, 80)
-	as.Equal(config.Server.Timeout, 20)
+func TestLoader_Load_HugeFile(t *testing.T) {
+	as := assert.New(t)
+
+	type Level5Config struct {
+		Key        string   `mapstructure:"key"`
+		Pi         float64  `mapstructure:"pi"`
+		Enabled    bool     `mapstructure:"enabled"`
+		List       []string `mapstructure:"list"`
+		AnotherKey string   `mapstructure:"anotherKey"`
+	}
+
+	type Level4Config struct {
+		Level5 Level5Config `mapstructure:"level5"`
+	}
+
+	type Level3Config struct {
+		Level4 Level4Config `mapstructure:"level4"`
+	}
+
+	type Level2Config struct {
+		Level3 Level3Config `mapstructure:"level3"`
+	}
+
+	type Level1Config struct {
+		Level2 Level2Config `mapstructure:"level2"`
+	}
+
+	type NestedConfig struct {
+		Level1 Level1Config `mapstructure:"level1"`
+	}
+
+	type OAuthConfig struct {
+		ClientID     string `mapstructure:"clientId"`
+		ClientSecret string `mapstructure:"clientSecret"`
+	}
+
+	type ThirdPartyConfig struct {
+		Google   OAuthConfig `mapstructure:"google"`
+		Facebook OAuthConfig `mapstructure:"facebook"`
+	}
+
+	type LogFile struct {
+		Path       string `mapstructure:"path"`
+		MaxSize    string `mapstructure:"maxsize"`
+		MaxBackups int    `mapstructure:"maxbackups"`
+		MaxAge     string `mapstructure:"maxage"`
+	}
+
+	type LogConfig struct {
+		Level  string   `mapstructure:"level"`
+		Format string   `mapstructure:"format"`
+		Output []string `mapstructure:"output"`
+		File   LogFile  `mapstructure:"file"`
+	}
+
+	type SSLConfig struct {
+		Enabled         bool   `mapstructure:"enabled"`
+		CertificatePath string `mapstructure:"certificatePath"`
+		PrivateKeyPath  string `mapstructure:"privateKeyPath"`
+	}
+
+	type CorsConfig struct {
+		Enabled        bool     `mapstructure:"enabled"`
+		AllowedOrigins []string `mapstructure:"allowedOrigins"`
+		MaxAge         string   `mapstructure:"maxAge"`
+	}
+
+	type ServerConfig struct {
+		Host string     `mapstructure:"host"`
+		Port int        `mapstructure:"port"`
+		Cors CorsConfig `mapstructure:"cors"`
+		SSL  SSLConfig  `mapstructure:"ssl"`
+	}
+
+	type DatabaseConfig struct {
+		Username string `mapstructure:"username"`
+		Password string `mapstructure:"password"`
+		Host     string `mapstructure:"host"`
+		Port     int    `mapstructure:"port"`
+		Name     string `mapstructure:"name"`
+		Timeout  string `mapstructure:"timeout"`
+	}
+
+	type Configuration struct {
+		Database      DatabaseConfig   `mapstructure:"database"`
+		Server        ServerConfig     `mapstructure:"server"`
+		Logs          LogConfig        `mapstructure:"logs"`
+		ThirdParty    ThirdPartyConfig `mapstructure:"thirdParty"`
+		NestedExample NestedConfig     `mapstructure:"nestedExample"`
+	}
+
+	type AppMetadata struct {
+		Author string `mapstructure:"author"`
+		Email  string `mapstructure:"email"`
+		Year   int    `mapstructure:"year"`
+	}
+
+	type AppConfig struct {
+		Name        string        `mapstructure:"name"`
+		Version     string        `mapstructure:"version"`
+		Description string        `mapstructure:"description"`
+		Metadata    AppMetadata   `mapstructure:"metadata"`
+		Features    []string      `mapstructure:"features"`
+		Config      Configuration `mapstructure:"config"`
+	}
+
+	type App struct {
+		App AppConfig `mapstructure:"app"`
+	}
+
+	loader := NewLoader[App]()
+
+	config, err := loader.Load("huge", YamlExt, "./testdata/loadYaml")
+
+	as.Nil(err)
+	as.Equal(config, &App{
+		App: AppConfig{
+			Name:        "My Super Application",
+			Version:     "v1.2.3",
+			Description: "This is a very long description of the application. It spans multiple lines and is intended to give an overview of what the application is and does.",
+			Metadata: AppMetadata{
+				Author: "Jane Doe",
+				Email:  "jane.doe@example.com",
+				Year:   2023,
+			},
+			Features: []string{"authentication", "logging", "analytics"},
+			Config: Configuration{
+				Database: DatabaseConfig{
+					Username: "dbuser",
+					Password: "securepassword",
+					Host:     "localhost",
+					Port:     5432,
+					Name:     "mydatabase",
+					Timeout:  "10s",
+				},
+				Server: ServerConfig{
+					Host: "0.0.0.0",
+					Port: 8080,
+					Cors: CorsConfig{
+						Enabled:        true,
+						AllowedOrigins: []string{"https://example1.com", "https://example2.com"},
+						MaxAge:         "300s",
+					},
+					SSL: SSLConfig{
+						Enabled:         false,
+						CertificatePath: "/etc/ssl/certs/cert.pem",
+						PrivateKeyPath:  "/etc/ssl/private/key.pem",
+					},
+				},
+				Logs: LogConfig{
+					Level:  "info",
+					Format: "json",
+					Output: []string{"stdout", "file"},
+					File: LogFile{
+						Path:       "/var/log/myapp.log",
+						MaxSize:    "100MB",
+						MaxBackups: 5,
+						MaxAge:     "30d",
+					},
+				},
+				ThirdParty: ThirdPartyConfig{
+					Google: OAuthConfig{
+						ClientID:     "google-client-id",
+						ClientSecret: "google-client-secret",
+					},
+					Facebook: OAuthConfig{
+						ClientID:     "facebook-app-id",
+						ClientSecret: "facebook-app-secret",
+					},
+				},
+				NestedExample: NestedConfig{
+					Level1: Level1Config{
+						Level2: Level2Config{
+							Level3: Level3Config{
+								Level4: Level4Config{
+									Level5: Level5Config{
+										Key:        "deeply nested value",
+										Pi:         3.14159265359,
+										Enabled:    true,
+										List:       []string{"item1", "item2", "item3"},
+										AnotherKey: "This is another long text that is associated with 'anotherKey' in this deeply nested structure.\n",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	})
 }
